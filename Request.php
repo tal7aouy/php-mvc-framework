b023d4230ba35d16e58ca3370427d8484a364155
tal7aouy <tal7aouy@gmail.com>
2022-04-12 13:07:02 +0000

    support json

--- Request.php
+++ Request.php
@@ -45,4 +45,18 @@ class Request
         }
         return $body;
     }
+    /**
+     * Get json as associative array or object from request body
+     * Content-Type header should be application/json in HTTP POST
+     * @param boolean $associative When true, JSON objects will be returned as associative arrays; when false, JSON objects will be returned as objects.
+     * @param integer $flags 
+     * @param integer $depth Maximum nesting depth of the structure being decoded.
+     * @return mixed
+     */
+    public function body(bool $associative = true, int $flags = 0, int $depth = 512)
+    {
+        $body = file_get_contents("php://input");
+        $object = json_decode($body, $associative, $depth, $flags);
+        return $object;
+    }
 }
--- Response.php
+++ Response.php
@@ -13,6 +13,19 @@ class Response
 
     public function redirect(string $url)
     {
-        header('Location: '.$url);
+        header("Location: ${url}");
+    }
+
+    /**
+     * Return json
+     *
+     * @param array $obj The associative array to return as json
+     * @param integer $flags 
+     * @param integer $depth Set the maximum depth. Must be greater than zero.
+     * @return string
+     */
+    public function json(array $obj, int $flags = 0, int $depth = 512): string
+    {
+        return json_encode($obj, $flags, $depth);
     }
 }
